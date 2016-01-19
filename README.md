12403768d47aa928063564ab36700d92c810f081
vanik <manachyn@gmail.com>
2016-01-19 16:55:28 +0200

    to public

---
+++ README.md
@@ -0,0 +1,10 @@
+# Reddit API
+
+### Technologies
+
+- [React](https://facebook.github.io/react/)
+- [Redux](http://rackt.github.io/redux)
+
+### Tools
+- [Webpack](https://webpack.github.io/) - module bundler.
+- [Babel](https://github.com/babel/babel/) - The compiler for writing next generation JavaScript.
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    devtool: 'cheap-module-eval-source-map',
+    entry: [
+        'webpack-hot-middleware/client',
+        './index'
+    ],
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/static/'
+    },
+    plugins: [
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin()
+    ],
+    module: {
+        loaders: [
+            { test: /\.jsx?$/, exclude: /(node_modules|bower_components)/, loader: 'babel' }
+        ]
+    }
+};
